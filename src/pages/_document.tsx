05c9b8c934e4782c4bbf9856607c451a085dcb6a
leo <celsolnv27@gmail.com>
2022-02-10 22:38:09 -0300

    fix: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" type="image/svg+xml" href="logo.svg" />
+          <title>Upfi</title>
         </Head>
         <body>
           <Main />
